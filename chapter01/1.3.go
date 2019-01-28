63f0cfb9a0edfabfa4d58b9390cc2ce8c7de32c3
Austin Schaffer <schaffer.austin.t@gmail.com>
2019-01-28 11:39:13 -0500

    Disclaimer for URLIfy

--- chapter01/1.3.go
+++ chapter01/1.3.go
@@ -6,6 +6,8 @@ import (
 
 // Trims the whitespace from the input string, and replaces the internal spaces
 // with `"%20"`.
+//
+// The result of this function is not actually URL-safe.
 func URLIfy(someString string) string {
 	someString = strings.TrimSpace(someString)
 
